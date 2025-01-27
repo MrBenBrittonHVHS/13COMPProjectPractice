83bb54d609e880758d1a016121493e0cf6f0d7c1
MrBenBrittonHVHS <106791155+MrBenBrittonHVHS@users.noreply.github.com>
2025-01-27 22:14:08 +1300

    Create fb_setup.js

---
+++ fb_setup.js
@@ -0,0 +1,29 @@
+var database;
+
+/**************************************************************/
+// fb_initialise()
+// Initialize firebase, connect to the Firebase project.
+// 
+// Find the config data in the Firebase consol. Cog wheel > Project Settings > General > Your Apps > SDK setup and configuration > Config
+//
+// Input:  n/a
+// Return: n/a
+/**************************************************************/
+function fb_initialise() {  
+const firebaseConfig = {
+    apiKey: "AIzaSyC_YI2XkIRIFNFr3Ivxjd-5wx5kxAOyrj8",
+    authDomain: "fir-rebuild-b3c92.firebaseapp.com",
+    databaseURL: "https://fir-rebuild-b3c92-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "fir-rebuild-b3c92",
+    storageBucket: "fir-rebuild-b3c92.appspot.com",
+    messagingSenderId: "187367274216",
+    appId: "1:187367274216:web:c399d98ffaadb217befb5d"
+};
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  // This log prints the firebase object to the console to show that it is working.
+  // As soon as you have the script working, delete this log.
+  console.log(firebase);	
+}
