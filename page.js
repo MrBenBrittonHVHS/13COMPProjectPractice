01bff7ae59bf0facb783dcd890ae49772bdaed11
MrBenBrittonHVHS <106791155+MrBenBrittonHVHS@users.noreply.github.com>
2025-01-30 03:26:08 +0000

    Jan 30, 2025, 3:26 AM

--- page.js
+++ page.js
@@ -30,7 +30,7 @@ function page_drawGame(gameState){
   <h1>${owner} vs. ${challenger}</h1>
   Make your guess!
 <br>
-<input type="number" id=guess></input><br>
-<button onclick="fb_makeGuess(${guess.value})">Guess</button>
+<input type="number" id="guess"></input><br>
+<button onclick="fb_makeGuess()">Guess</button>
   `
 }
