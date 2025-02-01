4172c2e84c9c12efdf74286f8eb6107c926ae257
MrBenBrittonHVHS <106791155+MrBenBrittonHVHS@users.noreply.github.com>
2025-02-01 22:52:00 +0000

    Feb 1, 2025, 10:52 PM

--- signup.js
+++ signup.js
@@ -2,6 +2,16 @@ fb_initialise();
 var user;
 fb_authenticate(()=>{});
 function signup_register(){
+
+
+    console.log (displayName.value);
+    console.log (age.value);
+    console.log (gender.value);
+    console.log (user.uid);
+    console.log (user.photoURL);
+
+
+
     var userDetails = {
         displayName:displayName.value,
         age:age.value,
@@ -9,5 +19,5 @@ function signup_register(){
         name:user.displayName,
         photoURL:user.photoURL
     }
-    firebase.database().ref('/userdetails/'+user.uid+'/').set(userDetails, fb_error());
+    firebase.database().ref('/userdetails/'+user.uid+'/').set(userDetails, fb_error);
 }
