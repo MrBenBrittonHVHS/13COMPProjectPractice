729ab0e1a1801dcdaa2e3782b3217a617e30fdf4
MrBenBrittonHVHS <106791155+MrBenBrittonHVHS@users.noreply.github.com>
2025-02-01 19:29:32 +0000

    Feb 1, 2025, 7:29 PM

--- GTN.js
+++ GTN.js
@@ -165,7 +165,7 @@ function gtn_updateScore(){
       if(theirID in scores && "wins" in scores[theirID]){
         theirLosses = scores[theirID].wins
       }
-      
+      console.log("update...")
       firebase.database().ref('/gameScores/GTN/').update(
         {
           [user.uid]: {
