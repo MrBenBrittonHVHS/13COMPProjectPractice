8862a3dc029bcb4a53f58ae019d63a9f411a3458
MrBenBrittonHVHS <106791155+MrBenBrittonHVHS@users.noreply.github.com>
2025-02-01 21:52:38 +0000

    Feb 1, 2025, 9:52 PM

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
 fb_initialise();
-fb_authenticate();
+var user;
+fb_authenticate(readUserDetails);
 
 function readUserDetails(){
-    firebase.database().ref('/gamesInProgress/'+gameID+'/').once('value', _readPlayers);
-    var theirID;
-    function _readPlayers(snapshot){
-      console.log("callback in gtn_updateScore: _readPlayers")
-  
-      if(snapshot.val().P1 == user.uid)
-        //I am player 1;
-        theirID = snapshot.val().P2;
-      else{
-        theirID = snapshot.val().P1;
-      }
-      console.log("MyID = "+user.uid)
-      console.log("TheirID = "+theirID)
-      firebase.database().ref('/gameScores/GTN/').once('value', _readScores);
-  
-      function _readScores(snapshot){
+    firebase.database().ref('/userdetails/'+user.uid+'/').once('value', _readDetails);
+    function _readDetails(snapshot){
         if(snapshot.val() == null){
-          //ScoreTable is missing, rebuild!
-          console.log("Scores Table missing, rebuilding")
-          firebase.database().ref('/gameScores/GTN/').set({dummyID:"dummyScore"}).then(gtn_updateScore);
-        }else{
+            //User Doesn't exist send to sign up page
+            alert ("Go away")
+        }  else {
+            alert ("welcome")
+        }
+    }
 }
