80d6c2d70810ef174cf6cfabd6a8d40faa7a7de1
MrBenBrittonHVHS <106791155+MrBenBrittonHVHS@users.noreply.github.com>
2025-02-02 00:42:36 +0000

    Feb 2, 2025, 12:42 AM

--- GTN.js
+++ GTN.js
@@ -44,10 +44,6 @@ function gtn_readGamesList(snapshot) {
 function gtn_createGame(){
   console.log("gtn_createGame")
   firebase.database().ref('/waitingGames').off()
-  console.log("writing to waiting games")
-  console.log ('/HelloWorld/'+user.uid);
-  console.log(displayName)
-  firebase.database().ref('/HelloWorld/'+user.uid).set(displayName, fb_error);
 
   firebase.database().ref('/waitingGames/'+user.uid).set(displayName, fb_error);
   gameRole = "gameOwner";
